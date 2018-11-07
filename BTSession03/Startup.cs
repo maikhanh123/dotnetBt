30b1dcb5b4249163ba8cfef11b6f967671b47eaf
phankhoi654321 <phankhoi654321@gmail.com>
2018-11-07 09:45:16 +0700

    Add project files.

---
+++ BTSession03/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BTSession03.Startup))]
+namespace BTSession03
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
